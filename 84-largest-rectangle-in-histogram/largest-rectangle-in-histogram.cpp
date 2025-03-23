550bb63129c8eecbf3df7b28be805495970a0028
AtharvaWagh03 <151638167+AtharvaWagh03@users.noreply.github.com>
2025-03-23 22:36:46 +0530

    Time: 68 ms (5.13%) | Memory: 92 MB (10.75%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,59 +1,67 @@
 class Solution {
+
 private:
-    vector<int> nextSmallerEle(vector<int> arr, int n) {
+
+    vector<int> nextSmallerElement(vector<int> &arr, int n) {
         stack<int> s;
         s.push(-1);
+
         vector<int> ans(n);
 
         for(int i=n-1;i>=0;i--) {
             int curr = arr[i];
-            while(s.top()!=-1 && arr[s.top()]>=curr) {
+            while(s.top()!=-1 && arr[s.top()] >= curr) {
                 s.pop();
             }
-            //ans is top
             ans[i] = s.top();
             s.push(i);
         }
-        return ans;
-    }
 
-    vector<int> prevSmallerEle(vector<int> arr, int n) {
+        return ans;
+}
+
+vector<int> prevSmallerElement(vector<int> &arr, int n) {
         stack<int> s;
         s.push(-1);
+
         vector<int> ans(n);
 
         for(int i=0;i<n;i++) {
             int curr = arr[i];
-            while(s.top()!=-1 && arr[s.top()]>=curr) {
+            while(s.top()!=-1 && arr[s.top()] >= curr) {
                 s.pop();
             }
-            //ans is top
             ans[i] = s.top();
             s.push(i);
         }
+
         return ans;
-    }
+}
+
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n  = heights.size();
+
+        int n = heights.size();
 
         vector<int> next(n);
-        next = nextSmallerEle(heights,n);
-
         vector<int> prev(n);
-        prev = prevSmallerEle(heights,n);
+
+        next = nextSmallerElement(heights,n);
+        prev = prevSmallerElement(heights,n);
+
         int area = INT_MIN;
+
         for(int i=0;i<n;i++) {
             int l = heights[i];
-            
 
-            if(next[i]==-1) {
+            if(next[i] == -1) {
                 next[i] = n;
             }
 
             int b = next[i]-prev[i]-1;
 
             int newArea = l*b;
+
             area = max(area,newArea);
         }
         return area;
